9ae1d50fcddac3789331e56ad01ab76f57e94719
pramodkajla <pramodkajla@gmail.com>
2021-07-24 01:25:29 +0530

    Constructor added

--- src/base/Utility.java
+++ src/base/Utility.java
@@ -3,5 +3,6 @@ package base;
 public class Utility {
     public static void main(String[] args) {
         System.out.println("Utility class main method.");
+        System.out.println("Enhancement in main branch");
     }
 }
